8639c7ec3a43e369793a3995a8fd073df6548bef
hghania <hamzighania2@gmail.com>
2025-07-04 12:39:42 +0200

    First commit

---
+++ math.js
@@ -0,0 +1,13 @@
+function addition(a,b){
+
+    return a+b;
+}
+function division(x,y){
+    if(y===0){
+        throw new Error("division par 0 interdit")
+    }
+
+    return x/y;
+
+}
+module.exports{addition,division}
---
+++ math.test.js
@@ -0,0 +1,9 @@
+
+const {division, addition } = require('./math')
+test("teste de la" , ()=>{
+expect(division(4,2)).toBe(2)
+})
+
+test("teste de la" , ()=>{
+expect(addition(4,2)).toBe(6)
+})
