6340177c4d55b0322528e1c67e466a0543e0f0dd
hghania <hamzighania2@gmail.com>
2025-07-06 23:12:34 +0200

    test 3

--- math.js
+++ math.js
@@ -1,13 +1,12 @@
-function addition(a,b){
-
-    return a+b;
+function addition(a, b) {
+  return a + b;
 }
-function division(x,y){
-    if(y===0){
-        throw new Error("division par 0 interdit")
-    }
+function division(x, y) {
+  if (y === 0) {
+    throw new Error("division par 0 interdit");
+  }
 
-    return x/y;
-
+  return x / y;
 }
-module.exports ={addition , division}
+
+module.exports = { addition, division };
--- math.test.js
+++ math.test.js
@@ -1,9 +1,9 @@
+const { division, addition } = require("./math");
 
-const {division, addition } = require('./math')
-test("teste de la" , ()=>{
-expect(division(4,2)).toBe(2)
-})
+test("teste de la", () => {
+  expect(division(4, 2)).toBe(2);
+});
 
-test("teste de la" , ()=>{
-expect(addition(4,2)).toBe(6)
-})
+test("teste de la", () => {
+  expect(addition(4, 2)).toBe(6);
+});
